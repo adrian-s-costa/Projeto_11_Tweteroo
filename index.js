dcbe31aa6db51581a4970859012e141f815cd949
pink_glb <adriancosta1215@gmail.com>
2022-06-20 15:44:05 -0400

    todas as rotas funcionais

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import nodemon from "nodemon";
 
 const app = express();
 
@@ -8,11 +7,31 @@ app.use(cors());
 app.use(express.json());
 
 const users = [];
+const tweets = [];
+let name;
 
 app.post("/sign-up", (req, res)=>{
     const user = req.body;
+    name = req.body.username;
     users.push(user);
     res.send("OK");
 });
+
+app.post("/tweets", (req, res)=>{
+    let userWPic = users.find(user=>user.username == name);
+    tweets.push({
+        username: name,
+        avatar: userWPic.avatar,
+        tweet: req.body.tweet
+    })
+    
+    console.log(tweets)
+    res.send("OK");
+})
+
+app.get("/tweets", (req, res)=>{
+    let revTweets = [].concat(tweets).reverse();
+    res.send(revTweets.slice(0,10));
+})
 
 app.listen(5000);
